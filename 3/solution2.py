948c99c0f477eba04579d733ed2f9caf71e6ca31
Linh Nguyen <nguyenlinhlinh@hotmail.com>
2025-12-27 09:39:47 +0100

    feat: add day 3

--- 3/solution2.py
+++ 3/solution2.py
@@ -2,6 +2,7 @@
 # To get the largest number with 12 digits iterate 12 times.
 # For the first digit the search range should be from 0 to n - 11
 # For the second digit the search range should be from postion of first digit to n - number of digit left to choose + 1
+
 import heapq
 
 f = open("3/input.txt", "r")
