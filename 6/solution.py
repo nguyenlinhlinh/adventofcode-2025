c8fbc033016afec1850ab8b391bc1d5134ef8afb
Linh Nguyen <nguyenlinhlinh@hotmail.com>
2025-12-27 09:38:46 +0100

    feat: add day 6

--- 6/solution.py
+++ 6/solution.py
@@ -1,37 +1,34 @@
+def getInput(f):
+    total = 0
+    worksheet = []
+    operators = []
+    for line in f:
+        if line[0] in ["+", "*"]:
+            for i in line.strip().split(" "):
+                if i:
+                    operators.append(i)
+        else:
+            a = []
+            for i in line.strip().split(" "):
+                if i:
+                    a.append(int(i))
+            worksheet.append(a)
+    return worksheet, operators
 
-# f = open("6/input.txt", "r")
-f = open("6/simple.txt", "r")
-total = 0
-worksheet = []
-for line in f:
-    if line[0] in ["+", "-", "*"]:
-        a = []
-        for i in line.split(" "):
-            if i != "" and i != "\n":
-                a.append(i)
-        worksheet.append(a)
-    else:
-        a = []
-        for i in line.split(" "):
-            if i != "" and i != "\n":
-                a.append(int(i))
-        worksheet.append(a)
-for i in worksheet:
-    print(i)
-result = [i for i in worksheet[0]]
-print("result", result)
 
-for r in range(1, len(worksheet) - 1):
-    for c in range(len(worksheet[0])):
-        print("row", r, "col", c, result)
-        if worksheet[-1][c] == "+":
-            print("+", r, c, result[c], worksheet[r][c])
-            result[c] += worksheet[r][c]
-        elif  worksheet[-1][c] == "*":
-            print("*", r, c, result[c], worksheet[r][c])
-            result[c] *= worksheet[r][c]
-        print()
-print(sum(result))
+def getResult():
+    f = open("6/input.txt", "r")
+    # f = open("6/simple.txt", "r")
+    worksheet, operators = getInput(f)
+    result = [i for i in worksheet[0]]
+    for r in range(1, len(worksheet)):
+        for c in range(len(worksheet[0])):
+            if operators[c] == "+":
+                result[c] += worksheet[r][c]
+            elif operators[c] == "*":
+                result[c] *= worksheet[r][c]
+    return sum(result)
+print(getResult())
 
 # Solution 4583860641327
 
--- 6/solution2.py
+++ 6/solution2.py
@@ -18,44 +18,38 @@ def rotateWorksheet(worksheet):
         for c in range(0, len(worksheet[0])):
             rotated[c][r] = worksheet[r][c]
 
-    result = []
+    newWorksheet = []
     a = []
-    operator = []
-    for r in rotated:
-        print(r)
+    operators = []
     for row in rotated:
         nbr = ""
         if len(a) == 0:
-            a.append(row[-1])
-        for c in range(len(row) - 1):
-            if row[c] != "" and row[c] != "\n":
-                nbr += row[c]
-        if nbr.strip() == "":
-            result.append(a)
+            operators.append(row[-1])
+        nbr = "".join([row[c] for c in range(len(row) - 1)]).strip()
+        if nbr == "":
+            newWorksheet.append(a)
             a = []
         else:
-            a.append(int(nbr.strip(" ")))
-    result.append(a)
-    print("new")
-    for r in result:
-        print(r)
-    return result
+            a.append(int(nbr))
+    newWorksheet.append(a)
+    return newWorksheet, operators
 
 def getResult():
     result = 0    
-    # f = open("6/input.txt", "r")
-    f = open("6/simple.txt", "r")
+    f = open("6/input.txt", "r")
+    # f = open("6/simple.txt", "r")
     worksheet = getInput(f)
-    rotatedWorksheet = rotateWorksheet(worksheet)
-    for row in rotatedWorksheet:
+    rotatedWorksheet, operators = rotateWorksheet(worksheet)
+    for r in  range(len(rotatedWorksheet)):
+        row = rotatedWorksheet[r]
         total = row[1]
-        print(row)
-        for n in range(2, len(row)):
-            if row[0] == "*":
-                total *= row[n]
-            else:
-                total += row[n]
-        result += total
+        if operators[r] == '+':
+            result += sum(row)
+        else:
+            total = 1
+            for nbr in row:
+                total *= nbr
+            result += total
     return result
 
 print(getResult())
